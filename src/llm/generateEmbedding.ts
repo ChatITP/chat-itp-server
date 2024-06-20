41597405f0c973965d6df4a71a87b06c30438fa0
ziyuan-linn <zl4140@nyu.edu>
2024-06-20 11:37:03 -0400

    add vector search

--- src/llm/generateEmbedding.ts
+++ src/llm/generateEmbedding.ts
@@ -4,13 +4,14 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 
-async function generateEmbeddingVectors(text: string) {
+async function generateEmbedding(text: string) {
   type embeddingArray = { embedding: number[] }[];
 
   const output = (await replicate.run(
     "replicate/all-mpnet-base-v2:b6b7585c9640cd7a9572c6e129c9549d79c9c31f0d3fdce7baac7c67ca38f305",
     { input: { text } }
   )) as embeddingArray;
-  console.log(output[0].embedding);
   return output[0].embedding;
 }
+
+export default generateEmbedding;
