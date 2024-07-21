ee0733e3e73b9b36abce0361579749ad085bdb2a
ziyuan-linn <zl4140@nyu.edu>
2024-07-21 15:37:25 -0400

    change dev server port number

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.ENV !== "production") {
   const app = express();
 
   app.use("/api", createProxyMiddleware({ target: "http://localhost:3001", changeOrigin: true }));
-  app.use("/", createProxyMiddleware({ target: "http://localhost:3000", changeOrigin: true }));
+  app.use(
+    "/",
+    createProxyMiddleware({
+      target: "http://localhost:3002/",
+      changeOrigin: true,
+    })
+  );
 
   app.listen(8000, () => {
     console.log("Reverse proxy server running at http://localhost:8000");
